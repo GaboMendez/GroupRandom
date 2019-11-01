8a6e25af10ff010a280e4a00c6a6875d4839f2ca
AngelR24 <49293458+AngelR24@users.noreply.github.com>
2019-10-31 21:38:57 -0400

    Created Grouper Method

--- RandomGroup/RandomGroup/Program.cs
+++ RandomGroup/RandomGroup/Program.cs
@@ -18,5 +18,32 @@ namespace RandomGroup
             Console.WriteLine();
 
         }
+
+
+
+        public Dictionary<int,int> Grouper(List<string> repartirList, List<string> gruposList)
+        {
+            Dictionary<int,int> groupedDictionary = new Dictionary<int, int>();
+            int groupId = 0;
+
+            while (repartirList.Count > 0)
+            {
+                foreach (var VARIABLE in gruposList)
+                {
+                    Random random = new Random();
+                    int studentId = 0;
+                    if (repartirList.Count > 0)
+                    {
+                        studentId = random.Next(0, repartirList.Count);
+                    }
+                    groupedDictionary.Add(studentId, groupId);
+                    groupId++;
+                    repartirList.RemoveAt(studentId);
+                }
+            }
+
+            return groupedDictionary;
+        }
+
     }
 }
