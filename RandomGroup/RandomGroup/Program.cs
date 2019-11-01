390c9b667089141d4417c2e3f281daeb78816f06
GaboSchool <1071470@est.intec.edu.do>
2019-10-31 21:13:42 -0400

    Adding txt files to bin/Debug

--- RandomGroup/RandomGroup/Program.cs
+++ RandomGroup/RandomGroup/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace RandomGroup
     {
         static void Main(string[] args)
         {
+            string[] lines = File.ReadAllLines("Estudiantes.txt");
+            Console.WriteLine();
+
         }
     }
 }
